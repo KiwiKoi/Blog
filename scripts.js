dc30eb7bb6dba47bbdff806f1c35538ec10d81f9
Daniel <danielo.visage@gmail.com>
2020-05-23 10:13:12 +0200

    Refine style, added dynamic copyright year

--- scripts.js
+++ scripts.js
@@ -22,3 +22,9 @@ const navSlide = () => {
 }
 
 navSlide();
+
+document.addEventListener("DOMContentLoaded", function() {
+    const yearSpan = document.querySelector(".copyright-year");
+    const currentYear = new Date().getFullYear();
+    yearSpan.textContent = currentYear;
+});
