d47167540158cfd47bc3d5ca56d1c8df0e4eeadc
Daniel <danielo.visage@gmail.com>
2020-05-23 15:08:59 +0200

    font update, fix animation on resize, footer social-icons, remove contact button

--- scripts.js
+++ scripts.js
@@ -28,3 +28,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const currentYear = new Date().getFullYear();
     yearSpan.textContent = currentYear;
 });
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
