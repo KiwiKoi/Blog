fdb0e62848d45eff0e48f251a141fb34b095417b
Daniel <danielo.visage@gmail.com>
2020-06-16 13:31:06 +0200

    Update style

--- js/blog-app.js
+++ js/blog-app.js
@@ -1,5 +1,14 @@
 "use strict"
 
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const port = 3000
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
 let articles = getSavedArticles()
 
 const filters = {
